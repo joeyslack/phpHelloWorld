e383a098774bb71b53c85f93700b8c8054fe8e0f
Joseph Slack <josephdslack@gmail.com>
2015-07-18 17:43:57 -0700

    Final commit :sunglasses:

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,1 +1,36 @@
-3cfbe0e6-2daf-11e5-94e7-28cfe9218f7b
+<?php
+
+namespace public\HelloWorld;
+
+/**
+* HellowWorld class
+* Outputs hello world based on chosen output
+*
+*/
+class HelloWorld {
+    
+    private $output = "Hello World";
+
+    /**
+    * Update output if user supplies output override
+    * @var string $output 
+    */
+    function __construct($output = $this->output) {
+        if ($this->output != $output) {
+            $this->setOutput($output);
+        }
+    }
+
+    /**
+    * Sets the text to be output
+    * @var string $output 
+    */
+    public function setOutput($output) {
+        $this->output = $output;
+    }
+
+    //Echos the set output
+    public function hello() {
+        echo $this->output;
+    }
+}
